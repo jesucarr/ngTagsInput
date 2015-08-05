e99c987cf951dae10ca619bb64f93bc51d35424e
Jesús Carrera <jesus.carrera@frontendmatters.com>
2015-08-05 15:40:21 +0200

    support for extra atts in input field

--- src/tags-input.js
+++ src/tags-input.js
@@ -50,6 +50,9 @@
  * @param {string=} [tagsExtraAttrs=NA] Ads extra attributes to the .tags element in the template, useful to add
  *    extra directives or any other HTML tag needed to that element. The value should be an object where the keys are the
  *    attribute and the value the attribute value. For example tags-extra-attrs="{myelement: 'myvalue'}".
+  * @param {string=} [inputExtraAttrs=NA] Ads extra attributes to the .input element in the template, useful to add
+ *    extra directives or any other HTML tag needed to that element. The value should be an object where the keys are the
+ *    attribute and the value the attribute value. For example input-extra-attrs="{myelement: 'myvalue'}".
  */
 tagsInput.directive('tagsInput', function($timeout, $document, $parse, $window, tagsInputConfig, tiUtil) {
     function TagList(options, events, onTagAdding, onTagRemoving) {
@@ -251,6 +254,9 @@ tagsInput.directive('tagsInput', function($timeout, $document, $parse, $window, 
         compile: function(element, attrs) {
             if (attrs.tagsExtraAttrs) {
                 angular.element(element[0].getElementsByClassName('tags')).attr($parse(attrs.tagsExtraAttrs)());
+            }
+            if (attrs.inputExtraAttrs) {
+                angular.element(element[0].getElementsByClassName('input')).attr($parse(attrs.inputExtraAttrs)());
             }
             return this.link;
         },
